528a50fcc938db02e9a64e04ccf449978c7efe18
WooDaeHyun <po668312@naver.com>
2022-12-29 02:04:29 +0900

    feat: 뉴스데이터 리스트 만들기

---
+++ src/components/NewsList.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=5d575546096d4e82897bcb8f851ea94a"
+        );
+        setArticles(res.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => {
+        return <NewsItem key={article.url} article={article} />;
+      })}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
