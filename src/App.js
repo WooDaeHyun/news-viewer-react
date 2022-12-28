b9e3e826c1f7eb223e0e0e0c68ac10110145edd8
WooDaeHyun <po668312@naver.com>
2022-12-29 03:03:58 +0900

    feat: react router적용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import Categories from "./components/Categories";
-import NewsList from "./components/NewsList";
-import { useState, useCallback } from "react";
+import NewsPage from "./components/NewsPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [category, setCategory] = useState("all");
-  const onSelect = useCallback((category) => {
-    return setCategory(category);
-  }, []);
-
   return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 }
 
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-
+import { NavLink } from "react-router-dom";
 const categories = [
   {
     name: "all",
@@ -42,7 +42,7 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -54,35 +54,28 @@ const Category = styled.div`
     color: #495057;
   }
 
-  ${(props) => {
-    return (
-      props.active &&
-      css`
-        font-size: 600;
-        border-bottom: 2px solid #22b8cf;
-        color: #22b8cf;
-        &:hover {
-          color: #3bc9db;
-        }
-      `
-    );
-  }}
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
   }
 `;
 
-const Categories = ({ category, onSelect }) => {
+const Categories = () => {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
         <Category
           key={c.name}
-          active={category === c.name}
-          onClick={() => {
-            onSelect(c.name);
-          }}
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+          to={c.name === "all" ? "/" : `/${c.name}`}
         >
           {c.text}
         </Category>
